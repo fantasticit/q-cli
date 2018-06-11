9a32bae1e9f08c534e14d31bae3c4c62909f6f9a
Hy <hyiron@aliyun.com>
2018-06-12 00:07:34 +0800

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 const config = require(path.resolve(__dirname, '../package.json'))
 
-// 导出 package.json 的 作者 和 repos 字段
+// 导出 package.json 字段
 module.exports = {
   author: config.author,
   cliName: config.name,
